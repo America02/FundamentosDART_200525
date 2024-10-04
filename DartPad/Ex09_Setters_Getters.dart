33265f92e5e516e2dcc3540de5cb81a686a7fd18
America02 <americayaely.estudillolicona@utxicotepec.edu.mx>
2024-10-04 10:31:33 -0600

    PRACTICA 11
    Actualización del archivo Ex09

--- DartPad/Ex09_Setters_Getters.dart
+++ DartPad/Ex09_Setters_Getters.dart
@@ -1,30 +1,31 @@
-void main(){
-  final mySquare = Square(side:18);
+void main() {
+  final mySquare = Square(side: 18); // Usamos 'side' en lugar de '_side'
 
-  mySquare._side = -32;
   print("""
-  Lado del cuadrado: ${mySquare._side}
-  Area que ocupa: ${mySquare.calculateArea()}
-  """);
+Lado del Cuadrado: ${mySquare.side}
+Área que ocupa: ${mySquare.calculateArea()}""");
+
+  mySquare.side = -32; // Intento de asignar valor negativo (causará excepción)
 }
 
 class Square {
-  double _side; // Formula para calcular el Area lado*lado
-    Square({required double side})
-    :_side= side;
+  double _side; // Variable privada para almacenar el lado del cuadrado
+  Square({required double side}) : _side = side;
 
-  double get area{
-    return _side*_side;
-  }
+  double get area => _side * _side; // Getter para el área (lado * lado)
 
-  set side(double value){
+  // Getter para acceder al valor del lado
+  double get side => _side;
+
+  // Setter para modificar el valor del lado
+  set side(double value) {
     print("Asignando un nuevo valor al lado: $value");
-    if (value<0) throw 'El valor del lado debe ser mayor a 0.';
-
-    _side= value;
+    if (value < 0) throw 'El valor del lado debe ser mayor a 0.';
+    _side = value;
   }
 
-  double calculateArea(){
-    return area;
+  // Método para calcular el área
+  double calculateArea() {
+    return _side * _side;
   }
 }
